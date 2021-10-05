9496431e4d131d0674bacfe57a2f6086854fc2ef
ilfat Aliev <ialiev685@gmail.com>
2021-10-05 22:47:35 +0300

    finish

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const controlContacts = require("./contacts.js");
-const argv = require("yargs").argv;
+// const argv = require("yargs").argv;
 
-console.log(process.argv);
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -10,7 +20,7 @@ function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "get":
-      controlContacts.getContactById(id);
+      controlContacts.getContactById(processingId(id));
       break;
 
     case "add":
@@ -18,12 +28,17 @@ function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "remove":
-      controlContacts.removeContact(id);
+      controlContacts.removeContact(processingId(id));
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
+}
+
+function processingId(id) {
+  if (Number(id)) return Number(id);
+  return id;
 }
 
 invokeAction(argv);
